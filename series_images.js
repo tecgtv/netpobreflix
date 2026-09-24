3758ab32a12d93676650e065504d3efcd21b7b7f
tecgtv <tecgtv@gmail.com>
2026-09-24 00:56:16 -0300

    Add files via upload

---
+++ series_images.js
@@ -0,0 +1,6 @@
+/*
+  Configuração visual das séries.
+  As capas/prévias ficam separadas do catálogo de episódios.
+  Séries sem imagem local configurada são resolvidas pelo app.js via TVMaze.
+*/
+const seriesImagesCatalog = {"Bates Motel":{"poster":"https://assets.fanart.tv/preview/tv/262414/tvposter/the-bates-motel-535807a80ec93.jpg","backdrop":""},"Dr. House":{"poster":"https://media.kg-portal.ru/tv/h/housemd/posters/housemd_4.jpg","backdrop":"https://www.ecartelera.com/carteles-series/400/464/001.jpg"}};
